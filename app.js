aede70888b4e93ce9bb129cdcc5bbf03ba788791
garciaalondra04364-coder <garciaalondra04364@gmail.com>
2026-06-15 15:32:05 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Inicializar la librería de animaciones AOS
+AOS.init({
+    duration: 1000, /* La animación dura 1 segundo, haciéndola suave */
+    once: true,     /* La animación solo ocurre la primera vez que el usuario baja */
+    offset: 100     /* Las tarjetas aparecen un poco antes de estar completamente en pantalla */
+});
